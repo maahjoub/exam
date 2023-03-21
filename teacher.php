4ee0966e8ebf4e1b5b56882d7943d7084ab93377
maahjoub <am470593@gmail.com>
2023-03-21 16:57:21 +0200

    update2

--- teacher.php
+++ teacher.php
@@ -8,6 +8,7 @@ if ($_SERVER['REQUEST_METHOD'] == "POST") {
     $level = $_POST['level'];
     $degree = $_POST['degree'];
     $startDate = $_POST['startDate'];
+    dd( str_replace('/', '-', $startDate));
     $specialize = $_POST['specialize'];
     $section =  $_POST['section'];
     $work =  $_POST['work'];
@@ -34,11 +35,14 @@ if ($_SERVER['REQUEST_METHOD'] == "POST") {
         $errors['work'] = "هذا الحقل مطلوب";
     }
     if (empty($errors)) {
-        $stmt = $coon->prepare("INSERT INTO student (name, gender_id, country_id, email, password, type) 
-                        VALUES (?, ?, ?, ?, ?, ?)");
-        $stmt->execute([$name, $genders, $countery, $email, $hashPass, $type]);
+        $stmt = $coon->prepare("INSERT INTO teacher (`name`, `level`, `dgree`, `start-date` , `specialize`, `section_id`, `word`) 
+                        VALUES (?, ?, ?, ?, ?, ?, ?)");
+        $stmt->execute([$name, $level, $degree, $startDate, $specialize, $section, $work]);
+        dd($stmt);
         if ($stmt->rowCount() > 0) {
-            header('location: login.php');
+            $id = $coon->lastinsertId();
+            $teacher = first($coon, "teacher", $id, "id");
+            header('location: teacher.php?teacher=' . $teacher);
             exit();
         } else {
         }
@@ -58,25 +62,27 @@ $countreis = selectAll($coon, "country");
                 <label for="">الاسم </label>
                 <input type="text" name="name" value="<?= isset($_POST['name']) ? $_POST['name'] : '' ?>" placeholder="اكتب اسم المستخدم">
             </div>
-
+            <?= isset($errors['name']) ? "<div class='alert-danger'>" . $errors['name'] . "</div>" : '' ?>
             <div class="input-grpup">
                 <label for="">الدرجة العلمية </label>
                 <input type="text" name="level" value="<?= isset($_POST['level']) ? $_POST['level'] : '' ?>" placeholder="اكتب   الشهادة التعليمية   ">
             </div>
+            <?= isset($errors['level']) ? "<div class='alert-danger'>" . $errors['level'] . "</div>" : '' ?>
             <div class="input-grpup">
                 <label for="">الشهادة التعليمية </label>
                 <input type="text" name="degree" value="<?= isset($_POST['degree']) ? $_POST['degree'] : '' ?>" placeholder="اكتب الشهادة التعليمية   ">
             </div>
+            <?= isset($errors['degree']) ? "<div class='alert-danger'>" . $errors['degree'] . "</div>" : '' ?>
             <div class="input-grpup">
                 <label for=""> تاريخ بدء العمل </label>
                 <input type="text" id="datepicker" name="startDate" placeholder="تاريخ بدء العمل">
             </div>
-
+            <?= isset($errors['startDate']) ? "<div class='alert-danger'>" . $errors['startDate'] . "</div>" : '' ?>
             <div class="input-grpup">
                 <label for=""> التخصص </label>
                 <input type="text" name="specialize" value="<?= isset($_POST['specialize']) ? $_POST['specialize'] : '' ?>" placeholder=" اكتب التخصص ">
             </div>
-
+            <?= isset($errors['specialize']) ? "<div class='alert-danger'>" . $errors['specialize'] . "</div>" : '' ?>
             <div class="input-grpup">
                 <span></span>
                 <select name="section" <?= isset($_POST['section']) ? $_POST['section'] : '' ?> id="">
@@ -86,11 +92,12 @@ $countreis = selectAll($coon, "country");
                     <?php endforeach ?>
                 </select>
             </div>
-
+            <?= isset($errors['section']) ? "<div class='alert-danger'>" . $errors['section'] . "</div>" : '' ?>
             <div class="input-grpup">
                 <label for=""> جهة العمل </label>
                 <input type="text" name="work" value="<?= isset($_POST['work']) ? $_POST['work'] : '' ?>" placeholder="اكتب جهة العمل  ">
             </div>
+            <?= isset($errors['work']) ? "<div class='alert-danger'>" . $errors['work'] . "</div>" : '' ?>
             <div class="submit">
                 <button type="submit">موافق </button>
             </div>
